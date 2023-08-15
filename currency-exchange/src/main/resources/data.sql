c61bd8f5e0e891c010de64830621af7ba8fff684
Kristijan Stošić <nimeria83@yahoo.com>
2023-08-15 10:31:20 +0200

    Initial Project

---
+++ currency-exchange/src/main/resources/data.sql
@@ -0,0 +1,59 @@
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10000,'EUR','RSD', 117.49);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10001,'USD','RSD', 112.04);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10002,'GBP','RSD', 135.79);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10003,'CHF','RSD', 117.60);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10004,'RSD','CHF', 0.0085);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10005,'EUR','CHF', 1);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10006,'USD','CHF', 0.95);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10007,'GBP','CHF', 1.16);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10008,'CHF','USD', 1.05);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10009,'EUR','USD', 1.05);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10010,'GBP','USD', 1.21);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10011,'RSD','USD', 0.0090);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10012,'RSD','EUR', 0.0085);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10013,'USD','EUR', 0.95);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10014,'CHF','EUR', 1.00);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10015,'GBP','EUR', 1.16);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10016,'RSD','GBP', 0.0074);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10017,'USD','GBP', 0.82);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10018,'CHF','GBP', 0.87);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10019,'EUR','GBP', 0.86);
